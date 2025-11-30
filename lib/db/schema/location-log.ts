5f32e1e1457fffc5732ea4aa0ab7c5870c896e9a
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-11-30 21:27:02 +0530

    feat: done auth setup

--- lib/db/schema/location-log.ts
+++ lib/db/schema/location-log.ts
@@ -1,5 +1,6 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth-schema";
 import { location } from "./location";
 
 export const locationLog = sqliteTable("locationLog", {
@@ -8,6 +9,7 @@ export const locationLog = sqliteTable("locationLog", {
   description: text(),
   lat: real().notNull(),
   lng: real().notNull(),
+  userId: int().notNull().references(() => user.id),
   locationId: int().notNull().references(() => location.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
