0d6ddb19164295cec619c1c421659e6094c55da5
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-12-10 23:24:12 +0530

    Feat: added Slug genrator, toast

---
+++ app/components/app/toast.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{ errorToast?: string; type: "success" | "error" | "warning" | "info" }>();
+const emit = defineEmits(["remove"]);
+
+onMounted(() => {
+  setTimeout(() => {
+    emit("remove");
+  }, 3000);
+});
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="toast toast-top toast-end">
+      <div class="alert" :class="`alert-${props.type}`">
+        <span>{{ props.errorToast }}</span>
+      </div>
+    </div>
+  </Teleport>
+</template>
--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,9 @@
+import type { DrizzleError } from "drizzle-orm";
+
 import db from "~~/lib/db";
 import { location, locationInsertSchema } from "~~/lib/db/schema";
+import { and, eq } from "drizzle-orm";
+import slugGenrator from "slug";
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -24,11 +28,41 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const [created] = await db.insert(location).values({
-    ...parsedBody.data,
-    slug: parsedBody.data.name.toLowerCase().replace(/ /g, "-"),
-    userId: event.context.user.id,
-  }).returning();
+  const existingLocation = await db.query.location.findFirst({
+    where: and(eq(location.name, parsedBody.data.name), eq(location.userId, event.context.user.id)),
+  });
 
-  return created;
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "Location already exists!",
+    }));
+  }
+
+  let slug = slugGenrator(parsedBody.data.name);
+  const existing = await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  });
+  if (existing) {
+    const rand5CharId = Math.floor(Math.random() * 100000).toString().padStart(5, "0");
+    slug = `${slug}-${rand5CharId}`;
+  }
+
+  try {
+    const [created] = await db.insert(location).values({
+      ...parsedBody.data,
+      slug,
+      userId: event.context.user.id,
+    }).returning();
+    return created;
+  }
+  catch (e) {
+    const error = e as DrizzleError;
+    if (error.message.includes("Failed query: insert")) {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "Slug must be unique (location already exists)",
+      }));
+    }
+  }
 });
